730f4a7891b9757ea92bf15ad8ad4b35e3940bb8
rogi-rogi <yrkim6883@gmail.com>
2025-12-09 14:08:46 +0900

    사용자 단위 테스트

--- src/test/java/org/restapi/springrestapi/support/fixture/UserFixture.java
+++ src/test/java/org/restapi/springrestapi/support/fixture/UserFixture.java
@@ -13,4 +13,29 @@ public final class UserFixture {
             .profileImageUrl("http://img")
             .build();
     }
+
+    public static User persistedUser(Long id) {
+        return persistedUser().toBuilder()
+            .id(id)
+            .build();
+    }
+
+    public static User uniqueUser(String suffix) {
+        long now = System.nanoTime();
+        String token = Long.toString(now, 36); // compact alphanumeric
+        if (token.length() > 9) {
+            token = token.substring(token.length() - 9);
+        }
+        String nickname = ("u" + token);
+        if (nickname.length() > 10) {
+            nickname = nickname.substring(0, 10);
+        } else if (nickname.length() < 2) {
+            nickname = "u1";
+        }
+
+        return persistedUser().toBuilder()
+            .email("user_" + suffix + "_" + now + "@test.com")
+            .nickname(nickname)
+            .build();
+    }
 }
